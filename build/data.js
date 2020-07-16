6809c72f15cf99da2c0230bbfebc208afb8c5fab
Alexis Burgos <alexisburgosgtm@outlook.com>
2020-07-16 00:05:26 -0600

    grafico

--- build/data.js
+++ build/data.js
@@ -10,10 +10,10 @@ let data = [
         renglon: 419,
         fuente: 41,
         nombre:"MINISTERIO DE DESARROLLO SOCIAL",
-        vigente:"6,000,000,000.00",
-        comprometido:"1,685,859,000.00",
-        devengado:"1,685,859,000.00",
-        pagado:"1,685,859,000.00",
+        vigente:6000000000,
+        comprometido:1685859000.00,
+        devengado:1685859000.00,
+        pagado:1685859000.00,
         ejecucion:"28%"
 
     },
@@ -28,10 +28,10 @@ let data = [
         renglon: 419,
         fuente: 41,
         nombre:"MINISTERIO DE ECONOMÍA",
-        vigente:"2,000,000,000.00",
-        comprometido:"692,193,750.00",
-        devengado:"692,193,750.00",
-        pagado:"692,193,750.00",
+        vigente:2000000000,
+        comprometido:692193750.00,
+        devengado:692193750.00,
+        pagado:692193750.00,
         ejecucion:"35%"
 
     },
@@ -46,10 +46,10 @@ let data = [
         renglon: 691,
         fuente: 41,
         nombre:"OBLIGACIONES DEL ESTADO A CARGO DEL TESORO",
-        vigente:"2,730,000,000.00",
-        comprometido:"1,200,000,000.00",
-        devengado:"1,200,000,000.00",
-        pagado:"1,200,000,000.00",
+        vigente:2730000000,
+        comprometido:1200000000.00,
+        devengado:1200000000.00,
+        pagado:1200000000.00,
         ejecucion:"44%"
 
     },
--- build/index.js
+++ build/index.js
@@ -19,15 +19,57 @@ function getView(){
             <div class="modal fade" id="modalDetalles" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog modal-lg" role="document">
                     <div class="modal-content">
-                        <div class="modal-header bg-primary text-white">
+                        <div class="modal-header bg-primary text-white text-center">
                             <h3 id="lbTituloGrafico">Gráfico</h3>
                         </div>
-
                         <div class="modal-body">
-                          <div class="">
+                            <br>
+                            <div class="table-responsive  col-12">
+                                <table class="table table-responsive table-hover table-striped">
+                                    <thead class="bg-dark text-white">
+                                        <tr>
+                                            <td>Entidad</td>
+                                            <td>Nombre</td>
+                                            <td>Vigente</td>
+                                            <td>Comprometido</td>
+                                            <td>Devengado</td>
+                                            <td>Pagado</td>
+                                            <td>%Ejecución</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="tblA">
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                            <div class="table-responsive col-12">
+                                <table class="table table-responsive table-hover table-striped">
+                                    <thead class="bg-dark text-white">
+                                        <tr>
+                                            <td>PRG</td>
+                                            <td>SUBP</td>
+                                            <td>PROY</td>
+                                            <td>ACT</td>
+                                            <td>REN</td>
+                                            <td>FTE</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="tblB">
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
 
-                          
-                          </div>
+                        </div>
+                        <div class="modal-body">
+
+                            <canvas id="barChart"></canvas>
+                         
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-md btn-outline-success" data-dismiss="modal">Aceptar</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -38,6 +80,19 @@ function getView(){
     rootModal.innerHTML = view.modalDetalles();
 
 }
+
+function setMoneda(num,signo) {
+    num = num.toString().replace(/\$|\,/g, '');
+    if (isNaN(num)) num = "0";
+    sign = (num == (num = Math.abs(num)));
+    num = Math.floor(num * 100 + 0.50000000001);
+    cents = num % 100;
+    num = Math.floor(num / 100).toString();
+    if (cents < 10) cents = "0" + cents;
+    for (var i = 0; i < Math.floor((num.length - (1 + i)) / 3); i++)
+        num = num.substring(0, num.length - (4 * i + 3)) + ',' + num.substring(num.length - (4 * i + 3));
+    return (((sign) ? '' : '-') + signo + ' ' + num + ((cents == "00") ? '' : '.' + cents));
+};
 
 function getCard(no,entidad,programa,subprograma,proyecto,actividad,renglon,fuente,nombre,vigente,comprometido,devengado,pagado,ejecucion){
   
@@ -69,28 +124,28 @@ function getCard(no,entidad,programa,subprograma,proyecto,actividad,renglon,fuen
                             <br>${subprograma}
                             <br><br>
                             <b>Vigente</b>
-                            <br>${vigente}
+                            <br>${setMoneda(vigente,'')}
                         </div>
                         <div class="col">
                             <b>Proyecto</b>
                             <br>${proyecto}
                             <br><br>
                             <b>Comprometido</b>
-                            <br>${comprometido}
+                            <br>${setMoneda(comprometido,'')}
                         </div>
                         <div class="col">
                             <b>Actividad</b>
                             <br>${actividad}
                             <br><br>
                             <b>Devengado</b>
-                            <br>${devengado}
+                            <br>${setMoneda(devengado,'')}
                         </div>
                         <div class="col">
                             <b>Renglón</b>
                             <br>${renglon}
                             <br><br>
                             <b>Pagado</b>
-                            <br>${pagado}
+                            <br>${setMoneda(pagado,'')}
                         </div>
                         <div class="col">
                             <b>Fuente:</b>
@@ -100,7 +155,8 @@ function getCard(no,entidad,programa,subprograma,proyecto,actividad,renglon,fuen
                             <br>${ejecucion}
                         </div>
                         <div class="col">
-                            <button class="btn btn-secondary btn-sm btn-circle" onclick="getDetallesCard();">
+                            <button class="btn btn-secondary btn-sm btn-circle" onclick=
+                            "getDetallesCard(${no},'${entidad}','${programa}','${subprograma}','${proyecto}','${actividad}','${renglon}','${fuente}','${nombre}','${vigente}','${comprometido}','${devengado}','${pagado}','${ejecucion}');">
                                 +
                             </button>
                         </div>
@@ -115,12 +171,82 @@ function getCard(no,entidad,programa,subprograma,proyecto,actividad,renglon,fuen
         `
 };
 
-function getDetallesCard(){
+function getDetallesCard(no,entidad,programa,subprograma,proyecto,actividad,renglon,fuente,nombre,vigente,comprometido,devengado,pagado,ejecucion){
+    
+    document.getElementById('lbTituloGrafico').innerText= nombre;
 
+    //DETALLES DE LA TABLA INICIAL DEL MODAL
+    document.getElementById('tblA').innerHTML = `<tr>
+                                                    <td>${entidad}</td>
+                                                    <td>${nombre}</td>
+                                                    <td>${setMoneda(vigente,'')}</td>
+                                                    <td>${setMoneda(comprometido,'')}</td>
+                                                    <td>${setMoneda(devengado,'')}</td>
+                                                    <td>${setMoneda(pagado,'')}</td>
+                                                    <td>${ejecucion}</td>
+                                                </tr>`
+
+    document.getElementById('tblB').innerHTML = `<tr>
+                                                    <td>${programa}</td>
+                                                    <td>${subprograma}</td>
+                                                    <td>${proyecto}</td>
+                                                    <td>${actividad}</td>
+                                                    <td>${renglon}</td>
+                                                    <td>${fuente}</td>
+                                                </tr>`
+
+    // CARGANDO DATOS DE LA GRAFICA
+    let labels = [];
+    let dataset = [];
+    
+    labels.push("Vigente");
+    labels.push("Comprometido");
+    labels.push("Devengado");
+    labels.push("Pagado");
+
+    dataset.push(Number(vigente));
+    dataset.push(Number(comprometido));
+    dataset.push(Number(devengado));
+    dataset.push(Number(pagado));
+
+    var data = {
+        labels: labels,
+        datasets: [{
+        label: 'Q',
+        data: dataset,
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1,
+        fill: false
+        }]
+    };
+
+    var options = {
+        scales: {
+        yAxes: [{
+            ticks: {
+            beginAtZero: true
+            }
+        }]
+        },
+        legend: {
+        display: false
+        },
+        elements: {
+        point: {
+            radius: 0
+        }
+        }
+    };
+
+    var barChartCanvas = $("#barChart").get(0).getContext("2d");
+    var barChart = new Chart(barChartCanvas, {type: 'bar', data: data, options: options});
 
     $('#modalDetalles').modal('show');
 
 }
+
+
 
 function addListeners(){
 
